b4e8c0aef1f7315eb354ef6563e7601146df3bdd
Josh <zylo.codes@gmail.com>
2022-07-19 15:10:51 -0500

    Dark mode styles

--- client/src/ctx/themeContext.js
+++ client/src/ctx/themeContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 
 const defaultTheme = 'light'
 
@@ -6,6 +6,16 @@ export const ThemeContext = createContext(defaultTheme)
 
 export const ThemeProvider = props => {
   const [theme, setTheme] = useState(defaultTheme)
+
+  useEffect(() => {
+    if (theme === 'light') {
+      document.body.classList.remove('dark')
+      document.body.classList.add('light')
+    } else {
+      document.body.classList.remove('light')
+      document.body.classList.add('dark')
+    }
+  }, [theme])
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
